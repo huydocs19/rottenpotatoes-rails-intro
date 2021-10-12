7903ed33e8e809867b0e1dfbc170fea77922ea50
Huy Do <huyqdo@berkeley.edu>
2021-10-12 20:07:59 +0000

    make routes more RESTful

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,45 +8,44 @@ class MoviesController < ApplicationController
     @sorted_by = session[:sorted_by]
   end
 
-  def index    
-    if params[:ratings].nil? && params[:sorted_by].nil? && (
-      !session[:sorted_by].nil? || !session[:ratings].nil?)      
-      redirect_to movies_path(sorted_by: session[:sorted_by], ratings: session[:ratings].to_h { |rating| [rating, "1"] } )
+  def index      
+    
+    if params[:ratings].nil? && params[:sorted_by].nil? 
+      if !session[:sorted_by].nil? && !session[:ratings].nil?
+        redirect_to movies_path(sorted_by: session[:sorted_by], 
+        ratings: session[:ratings].to_h { |rating| [rating, "1"] } )
+      elsif !session[:sorted_by].nil? 
+        redirect_to movies_path(sorted_by: session[:sorted_by])
+      elsif !session[:ratings].nil?
+        redirect_to movies_path(ratings: session[:ratings].to_h { |rating| [rating, "1"] })
+      end
+      
     end    
     @all_ratings = Movie.all_ratings
-    if session[:sorted_by].nil?
+    if !params[:ratings].nil?
+      @ratings_to_show = params[:ratings].keys
+      session[:ratings] = @ratings_to_show          
+    else
+      @ratings_to_show = @all_ratings
+      session.delete(:ratings)
+    end
+    @movies = Movie.with_ratings(@ratings_to_show)
+    if !params[:sorted_by].nil?
+      @movies = @movies.sorted_by(params[:sorted_by]) || @movies  
+      session[:sorted_by] = params[:sorted_by]      
+    else
+      session.delete(:sorted_by)
+    end  
+    if params[:sorted_by].nil?
       @title_color = ""
       @release_date_color = ""
-    elsif session[:sorted_by] == 'title'
+    elsif params[:sorted_by] == 'title'
       @title_color = "hilite bg-warning"
       @release_date_color = ""
     else
       @title_color = ""
       @release_date_color = "hilite bg-warning"
     end
-    if params[:commit] == "Refresh"
-        if params[:ratings]
-          @ratings_to_show = params[:ratings].keys
-          session[:ratings] = @ratings_to_show          
-        else
-          session[:ratings] = []
-          @ratings_to_show = []
-        end
-    else
-        if session[:ratings].nil? || session[:ratings].empty?
-          @ratings_to_show = @all_ratings
-        else
-          @ratings_to_show = session[:ratings]
-        end
-    end    
-    @movies = Movie.with_ratings(@ratings_to_show)
-    if params[:sorted_by]
-      @movies = @movies.sorted_by(params[:sorted_by]) || @movies  
-      session[:sorted_by] = params[:sorted_by]      
-    else
-      @movies = @movies.sorted_by(session[:sorted_by]) || @movies  
-    end  
-    
     
     
     
